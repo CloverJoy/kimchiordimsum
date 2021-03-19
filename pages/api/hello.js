e2cd66fa742405790f642ef73a1929d3f1532145
Christoffel Angga <Chris.ang.dev@gmail.com>
2021-03-19 13:42:10 -0700

    External api call test with axios

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,13 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import axios from 'axios'
 
-export default (req, res) => {
-  res.status(200).json({ name: 'John Doe' })
+const getData = async () => {
+  const req = await axios.get('https://api.jikan.moe/v3/season/later')
+  const data = req.data
+  return data
+}
+
+export default async (req, res) => {
+  const data = await getData()
+  res.status(200).json(data)
 }
