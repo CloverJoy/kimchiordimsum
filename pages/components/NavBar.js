8e63f493ce34217ddf935e50af256c3915ea4329
Christoffel Angga <Chris.ang.dev@gmail.com>
2021-03-25 21:48:19 -0700

    Added footer component

--- pages/components/NavBar.js
+++ pages/components/NavBar.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import Image from 'next/image'
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
 import NavbarBrand from 'react-bootstrap/NavbarBrand'
@@ -7,7 +7,14 @@ import Style from '@styles/Navbar.module.css'
 const NavBar = () => {
   return (
     <Navbar variant="primary" className={Style.bg} expand="lg">
-      <NavbarBrand className={Style.brand}>Kimchi or DimSum</NavbarBrand>
+      <NavbarBrand className={Style.brand}>
+        <Image
+          src="/kndlogo.png"
+          alt="Kimchi or DimSum"
+          width="70"
+          height="30"
+        />
+      </NavbarBrand>
       <Nav className="mr-auto">
         <Nav.Link className={Style.link} href="#Home">Home</Nav.Link>
         <Nav.Link className={Style.link} href="#pricing">Watch</Nav.Link>
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import axios from 'axios'
 import Hero from '@components/Hero'
 import NavBar from '@components/NavBar'
 import HeadComponent from '@components/Head'
+import Footer from '@components/Footer'
 import Style from '@styles/Home.module.css'
 
 export default function Home({name}) {
@@ -10,6 +11,7 @@ export default function Home({name}) {
       <HeadComponent />
       <NavBar />
       <Hero />
+      <Footer />
     </div>
   )
 }
