89cbafa00a9178898303f39287694ee8ad1a0206
Christoffel Angga <Chris.ang.dev@gmail.com>
2021-03-28 00:24:46 -0700

    Succesfully encode html text

--- pages/components/watch/VideoPlayer.js
+++ pages/components/watch/VideoPlayer.js
@@ -1,14 +1,14 @@
 import Style from '@styles/VideoPlayer.module.css'
 import ResponsiveEmbed from 'react-bootstrap/ResponsiveEmbed'
 
-const VideoPlayer = () => (
+const VideoPlayer = ({video}) => (
   <div className={Style.videoplayer}>
     <ResponsiveEmbed aspectRatio="16by9">
-      <iframe className="embed-responsive-item" src={`https://www.youtube.com/embed/q9qN1hiO4dc`} allowFullScreen></iframe>
+      <iframe className="embed-responsive-item" src={`https://www.youtube.com/embed/${video.id.videoId}`} allowFullScreen></iframe>
     </ResponsiveEmbed>
     <div className={Style.videoplayerdetails}>
-      <h3>Hello hello</h3>
-    < div>description</div>
+      <h3 dangerouslySetInnerHTML={{__html: video.snippet.title}} />
+      <div dangerouslySetInnerHTML={{__html:video.snippet.description}} />
     </div>
   </div>
 )
--- pages/watch.js
+++ pages/watch.js
@@ -1,15 +1,27 @@
+import axios from 'axios'
 import NavBar from '@components/NavBar'
 import HeadComponent from '@components/Head'
 import Footer from '@components/Footer'
 import VideoList from '@components/watch/VideoList'
 import VideoPlayer from '@components/watch/VideoPlayer'
 
-export default function Watch() {
+export const getStaticProps = async () => {
+  const res = await axios.get('http://localhost:3000/api/ytviewcount')
+  const data = res.data.items;
+  return {
+    props: {
+      data: data
+    }
+  }
+}
+
+export default function Watch({data}) {
+  console.log(data)
   return (
     <div>
       <HeadComponent />
       <NavBar />
-      <VideoPlayer />
+      <VideoPlayer video={data[0]}/>
       <VideoList />
       <Footer />
     </div>
