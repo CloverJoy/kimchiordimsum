bd4ac0b210282b5fc1193738b658dc9789c7bded
Christoffel Angga <Chris.ang.dev@gmail.com>
2021-03-27 23:42:27 -0700

    Added bootstrap card for comparison`

--- pages/components/watch/VideoList.js
+++ pages/components/watch/VideoList.js
@@ -1,4 +1,5 @@
 import Style from '@styles/VideoList.module.css'
+import Card from 'react-bootstrap/Card'
 
 const VideoList = () => (
   <>
@@ -11,15 +12,16 @@ const VideoList = () => (
       <div className={Style.videolistentrydetail}>This is description example!</div>
     </div>
   </div>
-  <div className={Style.videolistentry}>
-    <div>
-      <img className={Style.media} src="https://i.ytimg.com/vi/YWhU7yRR4rM/default.jpg" alt="" />
-    </div>
-    <div>
-      <div className={Style.videolistentrytitle}>Itzy</div>
-      <div className={Style.videolistentrydetail}>This is description example!</div>
-    </div>
-  </div>
+  <Card style={{ width: '18rem' }} >
+    <Card.Img variant="top" src="https://i.ytimg.com/vi/YWhU7yRR4rM/default.jpg" />
+    <Card.Body>
+      <Card.Title className={Style.videolistentrytitle}>Card Title</Card.Title>
+      <Card.Text className={Style.videolistentrydetail}>
+        Some quick example text to build on the card title and make up the bulk of
+        the card's content.
+      </Card.Text>
+    </Card.Body>
+  </Card>
   </>
 );
 
